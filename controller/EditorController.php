98999ef35f87731e2d6ff2f7ab5611cd7dd7f753
Mariano Vargas <marianoluisvargas@gmail.com>
2024-06-14 15:46:10 -0300

    feat(Editor): se pueden borrar, activar y crear preguntas. editar preguntas no guarda la respuesta correcta, pero si cambia los contenidos de pregunta, respuesta y categoria

--- controller/EditorController.php
+++ controller/EditorController.php
@@ -33,26 +33,51 @@ class EditorController
         exit();
     }
 
-    public function deleteQuestion($id)
+    public function deleteQuestion()
     {
-        $this->model->deleteQuestion($id);
+        $idPregunta = $_GET['id'];
+        $this->model->deleteQuestion($idPregunta);
         header('Location: /editor');
         exit();
     }
 
-    public function addQuestion()
+    public function manageQuestion()
     {
-        // Aquí deberías manejar el formulario de agregar pregunta
-        // Este es un ejemplo simple
+        $idPregunta = isset($_GET['id']) ? $_GET['id'] : null;
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $pregunta = $_POST['pregunta'];
-            $respuestas = $_POST['respuestas']; // Este debería ser un array de respuestas
-            $this->model->addQuestion($pregunta, $respuestas);
+            $categoria = $_POST['categoria'];
+            $respuestas = array_values($_POST['respuestas']);
+            $correctaIndex = $_POST['correcta'];
+
+            foreach ($respuestas as $index => &$respuesta) {
+                $respuesta['correcta'] = ($index == $correctaIndex) ? 1 : 0;
+            }
+
+            if ($idPregunta) {
+                $this->model->updateQuestion($idPregunta, $categoria, $pregunta, $respuestas);
+            } else {
+                $this->model->addQuestion($categoria, $pregunta, $respuestas);
+            }
+
             header('Location: /editor');
             exit();
         }
 
         $data = [];
-        $this->presenter->render("view/addQuestionView.mustache", $data);
+        if ($idPregunta) {
+            $data['pregunta'] = $this->model->getQuestionById($idPregunta);
+            $data['respuestas'] = $this->model->getAnswers($idPregunta);
+        } else {
+            $data['respuestas'] = [
+                ['index' => 0, 'contenido' => '', 'correcta' => 0],
+                ['index' => 1, 'contenido' => '', 'correcta' => 0],
+                ['index' => 2, 'contenido' => '', 'correcta' => 0],
+                ['index' => 3, 'contenido' => '', 'correcta' => 0],
+            ];
+        }
+
+        $this->presenter->render("view/editor/manageQuestionView.mustache", $data);
     }
+
 }
--- model/EditorModel.php
+++ model/EditorModel.php
@@ -24,6 +24,15 @@ class EditorModel
         return $questions;
     }
 
+    public function getQuestionById($idPregunta)
+    {
+        $query = $this->database->prepare("SELECT * FROM Pregunta WHERE id = ?");
+        $query->bind_param("i", $idPregunta);
+        $query->execute();
+        $result = $query->get_result();
+        return $result->fetch_assoc();
+    }
+
     public function toggleQuestionState($idPregunta)
     {
         $query = $this->database->prepare("UPDATE Pregunta SET estado = NOT estado WHERE id = ?");
@@ -33,18 +42,46 @@ class EditorModel
 
     public function deleteQuestion($idPregunta)
     {
+        $query = $this->database->prepare("DELETE FROM Pregunta_respondida WHERE id_pregunta = ?");
+        $query->bind_param("i", $idPregunta);
+        $query->execute();
+
+        $query = $this->database->prepare("DELETE FROM Respuesta WHERE idPregunta = ?");
+        $query->bind_param("i", $idPregunta);
+        $query->execute();
+
         $query = $this->database->prepare("DELETE FROM Pregunta WHERE id = ?");
         $query->bind_param("i", $idPregunta);
         $query->execute();
     }
 
-    public function addQuestion($pregunta, $respuestas)
+    public function addQuestion($categoria, $pregunta, $respuestas)
     {
-        $query = $this->database->prepare("INSERT INTO Pregunta (pregunta) VALUES (?)");
-        $query->bind_param("s", $pregunta);
+        $query = $this->database->prepare("INSERT INTO Pregunta (categoria, contenido, estado) VALUES (?, ?, ?)");
+        $estado = 1;
+        $query->bind_param("ssi", $categoria, $pregunta, $estado);
         $query->execute();
 
         $idPregunta = $query->insert_id;
+
+        foreach ($respuestas as $respuesta) {
+            $contenido = $respuesta['contenido'];
+            $correcta = $respuesta['correcta'];
+            $query = $this->database->prepare("INSERT INTO Respuesta (contenido, idPregunta, correcta) VALUES (?, ?, ?)");
+            $query->bind_param("sii", $contenido, $idPregunta, $correcta);
+            $query->execute();
+        }
+    }
+
+    public function updateQuestion($idPregunta, $categoria, $pregunta, $respuestas)
+    {
+        $query = $this->database->prepare("UPDATE Pregunta SET categoria = ?, contenido = ? WHERE id = ?");
+        $query->bind_param("isi", $categoria, $pregunta, $idPregunta);
+        $query->execute();
+
+        $query = $this->database->prepare("DELETE FROM Respuesta WHERE idPregunta = ?");
+        $query->bind_param("i", $idPregunta);
+        $query->execute();
 
         foreach ($respuestas as $respuesta) {
             $contenido = $respuesta['contenido'];
