b31b719e1e47e196d8dd1953cd3b5e1edc343450
Mariano Vargas <marianoluisvargas@gmail.com>
2024-06-25 19:43:54 -0300

    update(validations)

--- model/UserModel.php
+++ model/UserModel.php
@@ -50,7 +50,7 @@ class UserModel
 
     public function register($username, $hashedPassword, $authToken, $fullName, $birthYear, $gender, $latitude, $longitude, $email, $profilePicturePath)
     {
-        $query = $this->database->prepare("INSERT INTO user (username, password, authToken, full_name, birth_year, gender, latitude, longitude, email, profile_picture) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+        $query = $this->database->prepare("INSERT INTO user (username, password, authToken, full_name, birth_year, gender, latitude, longitude, email, profile_picture, role) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, 'u')");
         $query->bind_param("ssssisiiss", $username, $hashedPassword, $authToken, $fullName, $birthYear, $gender, $latitude, $longitude, $email, $profilePicturePath);
         return $query->execute();
     }
