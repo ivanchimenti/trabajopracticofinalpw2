ad79b64a13cfcb643925368ca0c8787f12a4958d
Mariano Vargas <91701599+Rayvony@users.noreply.github.com>
2024-07-01 12:10:25 -0300

    Update RankingModel.php

--- model/RankingModel.php
+++ model/RankingModel.php
@@ -9,13 +9,21 @@ class RankingModel
         $this->database = $database;
     }
 
-    public function getRanking() {
-        return $this->database->query("SELECT username, MAX(puntuacion) AS puntuacion
-            FROM partida
-            GROUP BY username order by puntuacion desc;");
-//        return $this->database->query("SELECT ROW_NUMBER() OVER (ORDER BY puntuacion DESC) AS posicion, username, MAX(puntuacion) AS puntuacion
-//            FROM partida
-//            GROUP BY username;");
+    public function getRanking()
+    {
+        $query = $this->database->prepare("SELECT username, MAX(puntuacion) AS puntuacion FROM partida GROUP BY username ORDER BY puntuacion DESC;");
+        $query->execute();
+        $result = $query->get_result();
+
+        $ranking = [];
+        $position = 1;
+        while ($row = $result->fetch_assoc()) {
+            $row['posicion'] = $position++;
+            $ranking[] = $row;
+        }
+
+        return $ranking;
     }
+
 }
 
